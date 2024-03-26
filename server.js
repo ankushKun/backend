ac25ff570283b62a7bdc757acf257646b628b2d8
Ankush Singh <ankush4singh@gmail.com>
2024-03-26 23:39:20 +0530

    hello at base

--- server.js
+++ server.js
@@ -64,6 +64,10 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+app.get("/", (req, res) => {
+  res.json("Hello");
+}) 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
